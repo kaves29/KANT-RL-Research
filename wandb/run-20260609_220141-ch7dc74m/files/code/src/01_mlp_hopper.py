99a6184b32a01adcfc16d36668ae081539ba12c5
kaves29 <shourya23.kaveti@gmail.com>
2026-06-09 22:07:55 -0400

    Setup notebook 01
    Coded notebook 01, along with setting up wandb. Ready to run 10 seeds next session.

---
+++ wandb/run-20260609_220141-ch7dc74m/files/code/src/01_mlp_hopper.py
@@ -0,0 +1,32 @@
+import wandb
+import torch
+import numpy as np
+import gymnasium as gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+
+RAND_SEEDS = 1
+for seed in range(RAND_SEEDS):
+    # setting manual seeds to ensure reproducability
+    torch.manual_seed(seed)
+    np.random.seed(seed=seed)
+
+    # organizing results in wandb with seed identifier for reproducibility
+    run = wandb.init(
+        project="kant_rl_project",
+        name=f"MLP_Hopper_seed_{seed}",
+        save_code=True
+    )
+
+    env = gym.make("Hopper-v4", )
+    env.reset(seed=seed)
+
+    model = PPO("MlpPolicy", env, verbose=0, seed=seed)
+    model.learn(total_timesteps=1000000)
+
+    model_path = f"models/mlp_hopper/mlp_hopper_{seed}" # seed is included with each file name to create unique files
+    model.save(model_path)
+    run.log_model(path=model_path, name=f"MLP_Hopper_seed_{seed}")
+
+    env.close()
+    run.finish() # closing wandb file to stop and finalize recorded data
